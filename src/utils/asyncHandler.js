5163268b3aba432315b9bd5cd291268c8edc119b
vishnuprabhakar2001 <115687030+vishnuprabhakar2001@users.noreply.github.com>
2025-12-06 12:03:58 +0530

    utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+// const asyncHandler = () => {}         // This is just to understand how we pass the function inside the parameter.
+// const asyncHandler = (func) => { () => {}}  // So I could understand the below asyncHandler.
+// const asyncHandler = (func) =>  () => {}
+// const asyncHandler = (func) =>  async () => {}
+
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//        res.status(err.code || 500).json({
+//         success: false,
+//         message: err.message
+//        }) 
+//     }
+// }
