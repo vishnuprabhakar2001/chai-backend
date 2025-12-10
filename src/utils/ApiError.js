f17cd880d060d5e29f7dc510301a5d9bd5e52785
vishnuprabhakar2001 <115687030+vishnuprabhakar2001@users.noreply.github.com>
2025-12-10 18:42:45 +0530

    all setup configured

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         statusCode,
         message = "Something Went Wrong",
         errors = [],
-        statck = ""
+        stack = ""
      ){
         super(message)
         this.statusCode = statusCode
