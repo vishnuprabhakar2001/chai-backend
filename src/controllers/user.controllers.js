1de431d848b3f3ce7b70ffd7384b7c1115f62bf1
vishnuprabhakar2001 <115687030+vishnuprabhakar2001@users.noreply.github.com>
2025-12-15 22:58:21 +0530

    fixed few typo mistakes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -254,7 +254,11 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 const getCurrentUser = asyncHandler(async(req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "user fetched Successfully"
+    ))
 
 })
 
@@ -265,7 +269,7 @@ if (!fullName || !email) {
     throw new ApiError(400, "All fields are required")
 }
 
-const user = User.findByIdAndUpdate(
+const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
         $set: {
